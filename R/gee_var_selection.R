5a6642be193e24d5332f8a20ee04e5739e5e87b0
HanpuShen <105495679+HanpuShen@users.noreply.github.com>
2023-05-30 19:05:14 -0700

    Add files via upload

---
+++ R/gee_var_selection.R
@@ -0,0 +1,57 @@
+#' Variable Selection for GEE Logistic Regression Model Based on AIC...
+#'
+#' This function performs variable selection for a GEE logistic regression model using AIC as the selection criteria.
+#' @name Variable Selection for GEE Logistic Regression Model Based on AIC
+#' @param data A data frame containing the variables for the model.
+#' @param response The name of the response variable.
+#' @param id The name of the ID variable.
+#' @param family The family of the distribution (default is binomial).
+#' @param maxit The maximum number of iterations for the GEE algorithm (default is 25).
+#' @return The best GEE model according to AIC.
+#' @examples
+#' data <- data.frame(y = rbinom(100, 1, 0.5), x1 = rnorm(100), x2 = rnorm(100), id = rep(1:10, each = 10))
+#' gee_var_selection(data, "y", "id")
+#' @export
+# Load necessary libraries
+library(gee)
+library(MASS)
+
+compute_AIC <- function(model) {
+  k <- length(coef(model))
+  L <- sum(dbinom(model$y, size = 1, prob = model$fitted.values, log = TRUE))
+  AIC <- 2*k - 2*L
+  return(AIC)
+}
+
+# Define the function
+gee_var_selection <- function(data, response, id, family = binomial, maxit = 25) {
+  # Initialize variables
+  variables <- names(data)[-match(response, names(data))]
+  best_model <- NULL
+  best_aic <- Inf
+  
+  # Loop over all variable combinations
+  for (i in 1:length(variables)) {
+    combos <- combn(variables, i, simplify = FALSE)
+    for (combo in combos) {
+      # Build the model formula
+      formula <- as.formula(paste(response, '~', paste(combo, collapse = '+')))
+      
+      # Fit the GEE model
+      model <- gee(formula, id = id, data = data, family = family, maxit = maxit)
+      
+      # Calculate the AIC
+      aic <- compute_AIC(model)
+      # Update the best model if the current model is better
+      if (aic < best_aic) {
+        best_model <- model
+        best_aic <- aic
+        print(best_model)
+        print(best_aic)
+      }
+    }
+  }
+  
+  # Return the best model
+  return(best_model)
+}
